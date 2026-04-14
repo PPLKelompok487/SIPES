20314957286bbc9be5ad09bc93034af5909334c7
willsyh <willsyh25@gmail.com>
2026-04-14 23:23:12 +0700

    SIP-1 menambahkan fitur registrasi pengguna (FR-01)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Registration routes
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.post');
+
+// Login route (placeholder - you may need to create this)
+Route::get('/login', function() {
+    return '<h1>Halaman Login</h1><p>Fitur login akan dibuat selanjutnya</p><p><a href="/register">Kembali ke Register</a></p>';
+})->name('login');
